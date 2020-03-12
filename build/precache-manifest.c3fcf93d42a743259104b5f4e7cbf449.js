6dda50989ba775ccd66d1ab0b8df372eda45de37
ZhehaoTao <zhehaotao@gmail.com>
2020-03-12 16:51:30 +0800

    add build folder for github pages

---
+++ build/precache-manifest.c3fcf93d42a743259104b5f4e7cbf449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc771c1e415a30c2cf7c",
+    "url": "/monsters-rolodex/static/css/main.73744574.chunk.css"
+  },
+  {
+    "revision": "cc771c1e415a30c2cf7c",
+    "url": "/monsters-rolodex/static/js/main.2ce7875e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "66a77a6422f3c10af1977339ec939e12",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
